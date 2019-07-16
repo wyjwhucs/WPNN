fea8420479fd3c30b51aaf213dd74941a7d4c420
wyjwhucs <42434715+wyjwhucs@users.noreply.github.com>
2019-07-16 14:35:13 +0800

    Create TF_CNNs.py

---
+++ TF_CNNs.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import os
+import time
+import math
+import datetime
+import re
+import yaml
+import collections
+import numpy as np
+import tensorflow as tf
+from stop_words import get_stop_words
+from tensorflow.contrib import learn
